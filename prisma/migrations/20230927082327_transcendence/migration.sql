18a90b916572c55e127f60591bf0286d57210836
한규홍 <hankyuhong@kyhan-MacBookPro.local>
2023-09-27 17:25:02 +0900

    fix: database(users, friends)

--- prisma/migrations/20230927082327_transcendence/migration.sql
+++ prisma/migrations/20230927082327_transcendence/migration.sql
@@ -15,6 +15,7 @@ CREATE TABLE "Users" (
 CREATE TABLE "Friends" (
     "id" SERIAL NOT NULL,
     "user_id" INTEGER NOT NULL,
+    "friend_id" INTEGER NOT NULL,
 
     CONSTRAINT "Friends_pkey" PRIMARY KEY ("id")
 );
@@ -63,8 +64,14 @@ CREATE TABLE "Chat" (
     CONSTRAINT "Chat_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateIndex
+CREATE UNIQUE INDEX "Friends_user_id_friend_id_key" ON "Friends"("user_id", "friend_id");
+
 -- AddForeignKey
 ALTER TABLE "Friends" ADD CONSTRAINT "Friends_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Friends" ADD CONSTRAINT "Friends_friend_id_fkey" FOREIGN KEY ("friend_id") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "BlockedUsers" ADD CONSTRAINT "BlockedUsers_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
