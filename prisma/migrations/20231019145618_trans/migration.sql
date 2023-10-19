48d6ab99cd63b4285f8f94a6470a6b26f4f66ce3
roy0424 <roy0424@naver.com>
2023-10-20 00:11:46 +0900

    Add new migration

---
+++ prisma/migrations/20231019145618_trans/migration.sql
@@ -0,0 +1,149 @@
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('online', 'offline', 'in_game');
+
+-- CreateEnum
+CREATE TYPE "FriendRequestStatus" AS ENUM ('pending', 'accepted', 'declined');
+
+-- CreateEnum
+CREATE TYPE "ChannelOption" AS ENUM ('PUBLIC', 'PRIVATE', 'DM');
+
+-- CreateTable
+CREATE TABLE "Users" (
+    "id" INTEGER NOT NULL,
+    "name" VARCHAR(100),
+    "profile_image" VARCHAR(255),
+    "email" VARCHAR(255),
+    "is_2fa" BOOLEAN,
+    "status" "Status",
+
+    CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Friends" (
+    "id" SERIAL NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "friend_id" INTEGER NOT NULL,
+
+    CONSTRAINT "Friends_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "FriendRequests" (
+    "id" SERIAL NOT NULL,
+    "sender_id" INTEGER NOT NULL,
+    "receiver_id" INTEGER NOT NULL,
+    "status" "FriendRequestStatus" NOT NULL,
+
+    CONSTRAINT "FriendRequests_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "BlockedUsers" (
+    "id" SERIAL NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "blocked_by" INTEGER NOT NULL,
+
+    CONSTRAINT "BlockedUsers_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Channels" (
+    "id" SERIAL NOT NULL,
+    "password" VARCHAR(25) NOT NULL,
+    "name" VARCHAR(25) NOT NULL,
+
+    CONSTRAINT "Channels_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ChannelOptions" (
+    "channel_id" INTEGER NOT NULL,
+    "option" "ChannelOption",
+
+    CONSTRAINT "ChannelOptions_pkey" PRIMARY KEY ("channel_id")
+);
+
+-- CreateTable
+CREATE TABLE "ChannelUsers" (
+    "channel_id" INTEGER NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "admin" BOOLEAN NOT NULL,
+
+    CONSTRAINT "ChannelUsers_pkey" PRIMARY KEY ("user_id","channel_id")
+);
+
+-- CreateTable
+CREATE TABLE "ChannelBans" (
+    "channel_id" INTEGER NOT NULL,
+    "user_id" INTEGER NOT NULL,
+
+    CONSTRAINT "ChannelBans_pkey" PRIMARY KEY ("channel_id","user_id")
+);
+
+-- CreateTable
+CREATE TABLE "ChannelMutes" (
+    "channel_id" INTEGER NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "until" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "ChannelMutes_pkey" PRIMARY KEY ("channel_id","user_id")
+);
+
+-- CreateTable
+CREATE TABLE "Chat" (
+    "id" SERIAL NOT NULL,
+    "channel_id" INTEGER NOT NULL,
+    "sent_by_id" INTEGER NOT NULL,
+    "message" TEXT NOT NULL,
+
+    CONSTRAINT "Chat_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Users_name_key" ON "Users"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Friends_user_id_friend_id_key" ON "Friends"("user_id", "friend_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "FriendRequests_sender_id_receiver_id_key" ON "FriendRequests"("sender_id", "receiver_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "BlockedUsers_user_id_blocked_by_key" ON "BlockedUsers"("user_id", "blocked_by");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Channels_name_key" ON "Channels"("name");
+
+-- AddForeignKey
+ALTER TABLE "Friends" ADD CONSTRAINT "Friends_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Friends" ADD CONSTRAINT "Friends_friend_id_fkey" FOREIGN KEY ("friend_id") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "FriendRequests" ADD CONSTRAINT "FriendRequests_sender_id_fkey" FOREIGN KEY ("sender_id") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "FriendRequests" ADD CONSTRAINT "FriendRequests_receiver_id_fkey" FOREIGN KEY ("receiver_id") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "BlockedUsers" ADD CONSTRAINT "BlockedUsers_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "BlockedUsers" ADD CONSTRAINT "BlockedUsers_blocked_by_fkey" FOREIGN KEY ("blocked_by") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ChannelOptions" ADD CONSTRAINT "ChannelOptions_channel_id_fkey" FOREIGN KEY ("channel_id") REFERENCES "Channels"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ChannelUsers" ADD CONSTRAINT "ChannelUsers_channel_id_fkey" FOREIGN KEY ("channel_id") REFERENCES "Channels"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ChannelUsers" ADD CONSTRAINT "ChannelUsers_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Chat" ADD CONSTRAINT "Chat_channel_id_fkey" FOREIGN KEY ("channel_id") REFERENCES "Channels"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Chat" ADD CONSTRAINT "Chat_sent_by_id_fkey" FOREIGN KEY ("sent_by_id") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
