458b1c1f403b795109a0e597f94431666085c9d7
roy0424 <roy0424@naver.com>
2023-10-30 10:28:35 +0900

    Add NotificationGateway in chat module

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -3,9 +3,10 @@ import { ChatController } from './chat.controller';
 import { ChatService } from './chat.service';
 import { PrismaService } from '../prisma/prisma.service';
 import { TwoFaGuard } from '../auth/two-fa.guard';
+import { NotificationGateway } from '../notification/notification.gateway';
 
 @Module({
   controllers: [ChatController],
-  providers: [ChatService, PrismaService, TwoFaGuard],
+  providers: [ChatService, PrismaService, TwoFaGuard, NotificationGateway],
 })
 export class ChatModule {}
