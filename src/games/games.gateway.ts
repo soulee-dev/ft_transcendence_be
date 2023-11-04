637df0725650070345de9260601e2234d7d094c2
roy0424 <roy0424@naver.com>
2023-11-04 23:20:04 +0900

    Add game gateway

--- src/games/games.gateway.ts
+++ src/games/games.gateway.ts
@@ -2,13 +2,14 @@ import {
   ConnectedSocket,
   MessageBody,
   OnGatewayConnection,
+  OnGatewayDisconnect,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
 import { UseGuards } from '@nestjs/common';
 import { JwtWsGuard } from '../auth/jwt-ws.guard';
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 import { ExtendedSocket } from '../auth/jwtWsGuard.interface';
 import { GamesService } from './games.service';
 
@@ -19,79 +20,34 @@ import { GamesService } from './games.service';
   },
 })
 @UseGuards(JwtWsGuard)
-export class GamesGateway implements OnGatewayConnection {
+export class GamesGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
   constructor(private readonly gameService: GamesService) {}
-
-  handleConnection(client: ExtendedSocket): void {
-    console.log('Client connected:', client.id);
+  afterInit(server: Server) {
+    this.gameService.server = server;
   }
 
-  @SubscribeMessage('requestMatch')
-  async handleRequestMatch(
-    @ConnectedSocket() client: ExtendedSocket,
-    data: any,
-  ): Promise<void> {
-    try {
-      const playerId = client.user.sub;
-      const { game, matched } = await this.gameService.matchGame(playerId);
-
-      client.join(game.id.toString());
-      if (!matched) {
-        client.emit('InMatching', { message: '게임 매칭 중' });
-      }
-
-      if (matched) {
-        this.server
-          .to(game.id.toString())
-          .emit('matchSuccess', { message: '게임 매칭 성공' });
-        this.server
-          .to(game.id.toString())
-          .emit('start', { message: '게임 시작' });
-      }
-    } catch (error) {
-      console.error(error);
-      client.emit('matchError', { message: '게임 매칭 실패' });
-    }
+  handleConnection(client: Socket, ...args: any[]) {
+    console.log('a user connected');
   }
 
-  sendEndOfGame(gameId: number, winnerId: number): void {
-    this.server.to(gameId.toString()).emit('end', { winnerId });
+  handleDisconnect(client: Socket) {
+    console.log('user disconnected');
   }
 
-  @SubscribeMessage('cancelMatch')
-  async handleLeaveCurrentRoom(
-    @ConnectedSocket() client: ExtendedSocket,
-  ): Promise<void> {
-    try {
-      const rooms = this.server.sockets.adapter.sids.get(client.id);
-      if (rooms) {
-        for (const room of rooms.keys()) {
-          if (room !== client.id) {
-            const canceldMatch = await this.gameService.cancelMatch(
-              parseInt(room),
-            );
-            client.leave(room);
-          }
-        }
-      }
-    } catch (error) {
-      console.error(error);
-      client.emit('leaveError', { message: '매칭 취소 실패' });
-    }
+  @SubscribeMessage('join')
+  handleJoin(client: Socket) {
+    this.gameService.joinGame(client);
   }
 
-  @SubscribeMessage('leaveGame')
-  async handleLeaveGame(
-    @ConnectedSocket() client: ExtendedSocket,
-    @MessageBody() data: { gameId: number },
-  ): Promise<void> {
-    try {
-      client.leave(data.gameId.toString());
-    } catch (error) {
-      console.error(error);
-      client.emit('leaveError', { message: '게임 나가기 실패' });
-    }
+  @SubscribeMessage('move')
+  handleMove(client: Socket, payload: any) {
+    this.gameService.movePlayer(client, payload);
+  }
+
+  @SubscribeMessage('leave')
+  handleLeave(client: Socket, roomID: string) {
+    client.leave(roomID);
   }
 }
--- src/games/games.service.ts
+++ src/games/games.service.ts
@@ -1,9 +1,35 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { Server, Socket } from 'socket.io';
+
+interface Player {
+  socketID: string;
+  playerNo: number;
+  score: number;
+  x: number;
+  y: number;
+}
+
+interface Ball {
+  x: number;
+  y: number;
+  dx: number;
+  dy: number;
+}
+
+interface Room {
+  id: number;
+  players: Player[];
+  ball: Ball;
+  winner: number;
+}
 
 @Injectable()
 export class GamesService {
   constructor(private readonly prisma: PrismaService) {}
+
+  private rooms: Room[] = [];
+  public server: Server;
 
   async getRecord(id: number) {
     try {
@@ -76,5 +102,152 @@ export class GamesService {
       console.error(error);
       throw new BadRequestException(`게임 취소 실패`);
     }
+  }
+
+  joinGame(client: Socket) {
+    let room: Room | undefined = this.rooms.find((r) => r.players.length === 1);
+
+    if (room) {
+      client.join(room.id.toString());
+      client.emit('playerNo', 2);
+
+      room.players.push({
+        socketID: client.id,
+        playerNo: 2,
+        score: 0,
+        x: 690,
+        y: 200,
+      });
+
+      this.server.to(room.id.toString()).emit('startingGame');
+
+      setTimeout(() => {
+        this.server.to(room.id.toString()).emit('startedGame', room);
+        this.startGame(room);
+      }, 3000);
+    } else {
+      room = {
+        id: this.rooms.length + 1,
+        players: [
+          {
+            socketID: client.id,
+            playerNo: 1,
+            score: 0,
+            x: 90,
+            y: 200,
+          },
+        ],
+        ball: {
+          x: 395,
+          y: 245,
+          dx: Math.random() < 0.5 ? 1 : -1,
+          dy: 0,
+        },
+        winner: 0,
+      };
+      this.rooms.push(room);
+      client.join(room.id.toString());
+      client.emit('playerNo', 1);
+    }
+  }
+
+  movePlayer(
+    client: Socket,
+    data: { roomID: number; playerNo: number; direction: string },
+  ) {
+    const room = this.rooms.find((room) => room.id === data.roomID);
+
+    if (room) {
+      const player = room.players.find((p) => p.playerNo === data.playerNo);
+      if (player) {
+        if (data.direction === 'up') {
+          player.y -= 10;
+          player.y = Math.max(player.y, 0);
+        } else if (data.direction === 'down') {
+          player.y += 10;
+          player.y = Math.min(player.y, 440);
+        }
+        client.to(room.id.toString()).emit('updateGame', room);
+      }
+    }
+  }
+  startGame(room: Room) {
+    let interval = setInterval(() => {
+      // Ball movement logic
+      room.ball.x += room.ball.dx * 5;
+      room.ball.y += room.ball.dy * 5;
+
+      // Collision detection with paddles
+      const player1 = room.players[0];
+      const player2 = room.players[1];
+
+      // Player 1 collision
+      if (
+        room.ball.x < 110 &&
+        room.ball.y > player1.y &&
+        room.ball.y < player1.y + 60
+      ) {
+        room.ball.dx = 1;
+        this.adjustBallDirection(room.ball, player1.y);
+      }
+
+      // Player 2 collision
+      if (
+        room.ball.x > 670 &&
+        room.ball.y > player2.y &&
+        room.ball.y < player2.y + 60
+      ) {
+        room.ball.dx = -1;
+        this.adjustBallDirection(room.ball, player2.y);
+      }
+
+      // Wall collision detection
+      if (room.ball.y <= 5 || room.ball.y >= 485) {
+        room.ball.dy *= -1;
+      }
+
+      // Score update and ball reset
+      if (room.ball.x <= 5 || room.ball.x >= 795) {
+        this.updateScore(room);
+        this.resetBall(room);
+      }
+
+      // Emit the updated game state to all clients in the room
+      this.server.to(room.id.toString()).emit('updateGame', room);
+
+      // Check for win condition
+      if (player1.score >= 10 || player2.score >= 10) {
+        room.winner = player1.score >= 10 ? 1 : 2;
+        this.server.to(room.id.toString()).emit('endGame', room);
+        await this.prisma.games.create({});
+        clearInterval(interval);
+        this.rooms = this.rooms.filter((r) => r.id !== room.id); // Remove the room
+      }
+    }, 1000 / 60); // 60 times per second
+  }
+
+  private adjustBallDirection(ball: Ball, playerY: number) {
+    if (ball.y < playerY + 30) {
+      ball.dy = -1;
+    } else if (ball.y > playerY + 30) {
+      ball.dy = 1;
+    } else {
+      ball.dy = 0;
+    }
+  }
+
+  private updateScore(room: Room) {
+    if (room.ball.x <= 5) {
+      room.players[1].score += 1;
+    } else if (room.ball.x >= 795) {
+      room.players[0].score += 1;
+    }
+  }
+
+  private resetBall(room: Room) {
+    room.ball.x = 395;
+    room.ball.y = 245;
+    room.ball.dx = room.players[0].score >= 10 ? -1 : 1;
+    room.ball.dy = 0;
   }
 }
